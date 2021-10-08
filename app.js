61691a056364abd37865318dc27b3440ae8572b0
Your Name  git config --global user.email you@example.comgit config --global user.email holt.codes@gmail.com <you@example.com>
2021-10-07 17:55:34 -0700

    commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// Listen for submit
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+// Calculate Results
+function calculateResults(){
+  console.log('Calculating...');
+  // UI Vars
+  const amount = document.getElementById('amount');
+  const interest = document.getElementById('interest');
+  const years = document.getElementById('years');
+
+
+  e.preventDefault();
+}
